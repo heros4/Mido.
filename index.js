c3c29873ad96fe69f96aaac28cb4da2a68c58feb
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-27 19:56:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const youtube = new YouTube(GOOGLE_API_KEY);
 const queue = new Map();
 client.on('ready', function() {
 	console.log(`i am ready ${client.user.username}`);
-    client.user.setGame(prefix + 'Mido King | 3play |,`"https://www.twitch.tv/lboghaz_kingheros);
+    client.user.setGame(prefix + "3play");
 });
 
 
@@ -259,6 +259,33 @@ msg.author.send("Commands ستاتي " + `  **
 :headphones:  ${prefix}resume |لاعادت تشغيل الاغنية الموجودة
 **`);
  }
+});
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Mido King |`,"https://www.twitch.tv/lboghaz_kingheros")
+  console.log('')
+  console.log('')
+  console.log('+[-----------------------------------------------------------------]+')
+  console.log(`[Start] ${new Date()}`);
+  console.log('+[-----------------------------------------------------------------]+')
+  console.log('')
+  console.log('+[------------------------------------]+');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('By The Atlantic Enjoy')
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('+[------------------------------------]+')
+  console.log('')
+  console.log('+[------------]+')
+  console.log(' Bot Is Online')
+  console.log('+[------------]+')
+  console.log('')
+  console.log('')
 });
 
 
