abe3b0bb593c20f144e3dc31253d2282e90c56db
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-27 20:08:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,33 +14,11 @@ const PREFIX = '3';
 const youtube = new YouTube(GOOGLE_API_KEY);
 
 const queue = new Map();
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-	client.user.setGame(prefix + "3play | Mido King |`,"https://www.twitch.tv/lboghaz_kingheros")
-  console.log('')
-  console.log('')
-  console.log('+[-----------------------------------------------------------------]+')
-  console.log(`[Start] ${new Date()}`);
-  console.log('+[-----------------------------------------------------------------]+')
-  console.log('')
-  console.log('+[------------------------------------]+');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('By The Atlantic Enjoy')
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('+[------------------------------------]+')
-  console.log('')
-  console.log('+[------------]+')
-  console.log(' Bot Is Online')
-  console.log('+[------------]+')
-  console.log('')
-  console.log('')
+client.on('ready', function() {
+	console.log(`i am ready ${client.user.username}`);
+    client.user.setGame(prefix + 'help || 3play ');
 });
+
 
 
 client.on('warn', console.warn);
